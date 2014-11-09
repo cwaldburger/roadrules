42dacc7e48456eb8f7b19ffe11e45d8380cfd127
cwaldburger <cedric.waldburger@gmail.com>
2014-11-09 14:58:53 +0200

    initial commit

---
+++ 2 Web roadrules/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roadrules::Application.config.secret_key_base = 'febfc13f5ac2e41250d22802c6012a62914c9b778b5f459102eb501d98d79f9abddafe4d1ba58a7e6cd71046fae7e3c1a672bbf4caf0a128556a4ddc3ef56f38'
---
+++ 2 Web roadrules/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Roadrules::Application.config.session_store :cookie_store, key: '_roadrules_session'
